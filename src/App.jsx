0d4c15d745aa0896acdfc32a731ebdde554b4c86
Hashim7abdullah <hashim7abdullah7@gmail.com>
2024-11-15 17:28:09 +0530

    created

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,28 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import { Suspense } from "react";
+import { Suspense, lazy } from "react";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
-
-const Navbar = React.lazy(() => import("./Components/Navbar/Navabr"));
-const LandingPage = React.lazy(() => import("./Pages/LandingPage/LandingPage"));
+const Navbar = lazy(() => import("./Components/Navbar/Navabr"));
+const LandingPage = lazy(() => import("./Pages/LandingPage/LandingPage"));
+const Cart = lazy(() => import("./Pages/Cart/Cart.jsx"));
+const LoginForm = lazy(() => import("./Pages/Login/LoginForm"));
 
 const App = () => {
   return (
     <Router future={{ v7_startTransition: true, v7_relativeSplatPath: true }}>
-      <Suspense fallback={<div>Loading...</div>}>
+      <Suspense
+        fallback={
+          <div className="spinner"></div> // Show spinner while loading
+        }
+      >
         <ToastContainer />
         <Navbar />
         <Routes>
           <Route path="/" element={<LandingPage />} />
+          <Route path="/sign-in" element={<LoginForm />} />
+          <Route path="/cart" element={<Cart />} />
         </Routes>
       </Suspense>
     </Router>
