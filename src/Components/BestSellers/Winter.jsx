5a7fcf59e64f34823d8dc30678a184ec95d9c41a
Hashim7abdullah <hashim7abdullah7@gmail.com>
2024-11-18 12:40:50 +0530

    BESTSELLERS ADDED

---
+++ src/Components/BestSellers/Winter.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Title from "../Title/title"
+
+const Winter = () => {
+  return (
+    <div>
+      <div>
+        <Title text1={"Best"} text2={"Winter picks"} />
+      </div>
+      
+    </div>
+  );
+};
+
+export default Winter;
---
+++ src/assets/assets.js
@@ -0,0 +1,227 @@
+import img1 from "./bestSellers/hmgoepprod.jpg";
+import img2 from "./bestSellers/hmgoepprod (1).jpg";
+import img3 from "./bestSellers/hmgoepprod (2).jpg";
+import img4 from "./bestSellers/hmgoepprod (3).jpg";
+import img5 from "./bestSellers/hmgoepprod (4).jpg";
+import img6 from "./bestSellers/hmgoepprod (5).jpg";
+import img7 from "./bestSellers/hmgoepprod (6).jpg";
+import img8 from "./bestSellers/hmgoepprod (7).jpg";
+import img9 from "./bestSellers/hmgoepprod (8).jpg";
+import img10 from "./bestSellers/hmgoepprod (9).jpg";
+import img11 from "./bestSellers/hmgoepprod (10).jpg";
+import img12 from "./bestSellers/hmgoepprod (11).jpg";
+import img13 from "./bestSellers/hmgoepprod (12).jpg";
+import img14 from "./bestSellers/hmgoepprod (13).jpg";
+import img15 from "./bestSellers/hmgoepprod (14).jpg";
+import img16 from "./bestSellers/hmgoepprod (15).jpg";
+
+export const products = [
+  {
+    _id: "123",
+    name: "Oversized fleece hoodie",
+    description:
+      "Oversized hoodie in soft fleece with a zip at the top, gently dropped shoulders, long sleeves, and a rounded hem.",
+    price: 1999,
+    image: [img1],
+    category: "Women",
+    subCategory: "Topwear",
+    sizes: ["XS", "S", "M", "L", "XL"],
+    date: 1716634345448,
+    bestseller: true,
+  },
+  {
+    _id: "124",
+    name: "Pocket-detail cardigan",
+    description:
+      "Loose-fit cardigan in a dense knit with a round neckline and motif-detail metal buttons down the front. Dropped shoulders and flap chest pockets with a button.",
+    price: 2699,
+    image: [img2],
+    category: "Women",
+    subCategory: "Topwear",
+    sizes: ["XXS", "XS", "S", "M", "L", "XL"],
+    date: 1716634345448,
+    bestseller: true,
+  },
+  {
+    _id: "125",
+    name: "Bow-front rib-knit cardigan",
+    description:
+      "Long-sleeved cardigan in a soft rib knit with a round neckine and decorative bows and concealed press-studs at the front.",
+    price: 2299,
+    image: [img3],
+    category: "Women",
+    subCategory: "Topwear",
+    sizes: ["XXS", "XS", "S", "M", "L", "XL"],
+    date: 1716634345448,
+    bestseller: true,
+  },
+  {
+    _id: "126",
+    name: "Teddy jacket",
+    description:
+      "Loose-fit jacket in soft teddy with accent-colour trims. Round neckline, press-studs down the front, gently dropped shoulders and welt side pockets. Unlined.",
+    price: 2699,
+    image: [img4],
+    category: "Women",
+    subCategory: "Topwear",
+    sizes: ["XS", "S", "M", "L", "XL"],
+    date: 1716634345448,
+    bestseller: true,
+  },
+  {
+    _id: "127",
+    name: "Fringe-trimmed poncho",
+    description:
+      "Poncho in a patterned weave with an open front and fringes at the hem. Length at the back 65 cm.",
+    price: 1999,
+    image: [img5],
+    category: "Unisex",
+    subCategory: "Topwear",
+    sizes: ["XS", "S", "M", "L", "XL"],
+    date: 1716634345448,
+    bestseller: true,
+  },
+  {
+    _id: "128",
+    name: "Appliquéd jacket",
+    description:
+      "Loose-fit jacket in appliquéd fabric with a round neckline, concealed hook-and-eye fasteners down the front and shoulder pads for a defined silhouette. Jersey lining.",
+    price: 2999,
+    image: [img6],
+    category: "Women",
+    subCategory: "Topwear",
+    sizes: ["XXS", "XS", "S", "M", "L", "XL"],
+    date: 1716634345448,
+    bestseller: true,
+  },
+  {
+    _id: "129",
+    name: "Bead-embellished rib-knit cardigan",
+    description:
+      "Loose-fit cardigan in a soft rib knit featuring a round neckline and concealed button band down the front embellished with spherical beads. Gently dropped shoulders, long sleeves, and ribbing at the cuffs and hem.",
+    price: 2699,
+    image: [img7],
+    category: "Women",
+    subCategory: "Topwear",
+    sizes: ["XXS", "XS", "S", "M", "L", "XL"],
+    date: 1716634345448,
+    bestseller: true,
+  },
+  {
+    _id: "130",
+    name: "Double-breasted jacket",
+    description:
+      "Short, oversized jacket in woven fabric with a collar and a double-breasted front with covered buttons. Discreet pockets in the side seams and gently dropped shoulders. Lined.",
+    price: 2999,
+    image: [img8],
+    category: "Women",
+    subCategory: "Topwear",
+    sizes: ["XS", "S", "M", "L", "XL"],
+    date: 1716634345448,
+    bestseller: true,
+  },
+  {
+    _id: "131",
+    name: "Oversized printed sweatshirt",
+    description:
+      "Oversized top in printed sweatshirt fabric with a soft brushed inside. Round, rib-trimmed neckline, dropped shoulders and long sleeves. Ribbing at the cuffs and hem.",
+    price: 2299,
+    image: [img9],
+    category: "Women",
+    subCategory: "Topwear",
+    sizes: ["XXS", "XS", "S", "M", "L", "XL"],
+    date: 1716634345448,
+    bestseller: true,
+  },
+  {
+    _id: "132",
+    name: "Oversized zip-through hoodie",
+    description:
+      "Oversized, motif-detail zip-through hoodie in lightweight sweatshirt fabric. Lined hood, a concealed zip down the front, low dropped shoulders and long sleeves. Kangaroo pockets and ribbing at the cuffs and hem.",
+    price: 1999,
+    image: [img10],
+    category: "Unisex",
+    subCategory: "Topwear",
+    sizes: ["XXS", "XS", "S", "M", "L", "XL"],
+    date: 1716634345448,
+    bestseller: true,
+  },
+  {
+    _id: "133",
+    name: "Oversized embellished sweatshirt",
+    description:
+      "Oversized top in sweatshirt fabric with a soft brushed inside and embellishments at the front. Round, rib-trimmed neckline, dropped shoulders and long sleeves. Ribbing at the cuffs and hem.",
+    price: 2299,
+    image: [img11],
+    category: "Women",
+    subCategory: "Topwear",
+    sizes: ["XXS", "XS", "S", "M", "L", "XL"],
+    date: 1716634345448,
+    bestseller: true,
+  },
+  {
+    _id: "134",
+    name: "Oversized printed sweatshirt",
+    description:
+      "Oversized top in printed sweatshirt fabric with a soft brushed inside. Round, rib-trimmed neckline, long sleeves and ribbing at the cuffs and hem.",
+    price: 2299,
+    image: [img12],
+    category: "Unisex",
+    subCategory: "Topwear",
+    sizes: ["XXS", "XS", "S", "M", "L", "XL"],
+    date: 1716634345448,
+    bestseller: true,
+  },
+  {
+    _id: "135",
+    name: "Button-detail sweatshirt",
+    description:
+      "Loose-fit, slightly shorter top in sweatshirt fabric with a soft brushed inside. Gold-coloured metal buttons on one shoulder, low dropped shoulders and long sleeves. Ribbing around the neckline, cuffs and hem.",
+    price: 1999,
+    image: [img13],
+    category: "Women",
+    subCategory: "Topwear",
+    sizes: ["XXS", "XS", "S", "M", "L", "XL"],
+    date: 1716634345448,
+    bestseller: true,
+  },
+  {
+    _id: "136",
+    name: "Short puffer jacket",
+    description:
+      "Short puffer jacket in a quilted weave with a stand-up collar and a zip down the front. Welt front pockets and concealed elastication at the cuffs and hem. Lined.",
+    price: 2999,
+    image: [img14],
+    category: "Unisex",
+    subCategory: "Topwear",
+    sizes: ["XS", "S", "M", "L"],
+    date: 1716634345448,
+    bestseller: true,
+  },
+  {
+    _id: "137",
+    name: "Denim jacket",
+    description:
+      "Trucker-style jacket in rigid cotton denim with a collar, metal buttons down the front, forward-facing shoulder seams and a yoke at the front and back. Gently dropped shoulders, buttoned cuffs and flap chest pockets with a concealed press-stud.",
+    price: 3599,
+    image: [img15],
+    category: "Unisex",
+    subCategory: "Topwear",
+    sizes: ["XS", "S", "M", "L", "XL"],
+    date: 1716634345448,
+    bestseller: true,
+  },
+  {
+    _id: "138",
+    name: "Studded scuba sweatshirt",
+    description:
+      "Loose-fit sweatshirt in scuba fabric decorated with studs in various sizes at the top of the front. Round neckline, dropped shoulders and long sleeves.",
+    price: 2299,
+    image: [img16],
+    category: "Women",
+    subCategory: "Topwear",
+    sizes: ["XXS", "XS", "S", "M", "L", "XL"],
+    date: 1716634345448,
+    bestseller: true,
+  },
+];
